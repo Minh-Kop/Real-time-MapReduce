019e80679f3ea8765f8fb3a17342780852b294ca
20127506 <thoang1908@gmail.com>
2023-10-31 16:34:35 +0700

    4.2

--- mapreduce_file.py
+++ mapreduce_file.py
@@ -102,9 +102,76 @@ def Cuv_reducer():
         fw.writelines('{};{}\t{}\n'.format(
             user1, user2, str(len(a) - len(b))))
 
+    fw.close()
+
+
+def Suv_mapper():
+    data = read_file('./Cuv_reducer.txt')
+    fr1 = open('./I_reducer.txt', 'r')
+    fw = open('./Suv.txt', 'w')
+    I_list = []
+
+    for i in fr1:
+        user, val = i.strip().split('\t')
+        I, _ = val.strip().split(';')
+
+        I_list.append([user, I])
+
+    fr2 = open('./output_file.txt', 'r')
+    users = []
+
+    for i in fr2:
+        k, _ = i.strip().split('\t')
+        u, _ = k.strip().split(';')
+        users.append(int(u))
+
+    users = set(users)
+
+    for user, C in data:
+        u1, u2 = user.strip().split(';')
+
+        for i in I_list:
+            if (u1 == i[0]):
+                fw.writelines('{};{}\t{};{};{}\n'.format(u1, u2, C, u1, i[1]))
+            if (u2 == i[0]):
+                fw.writelines('{};{}\t{};{};{}\n'.format(u1, u2, C, u2, i[1]))
+
+    fr1.close()
+    fr2.close()
+    fw.close()
+
+
+def Suv_reducer():
+    data = read_file('./Suv.txt')
+    fw = open('./Suv_reducer.txt', 'w')
+    appended_list = []
+
+    for key, value in groupby(data, itemgetter(0)):
+        temp = list(value).copy()
+        user1, user2 = key.strip().split(';')
+
+        C, u1, I1 = temp[0][1].strip().split(';')
+        _, u2, I2 = temp[1][1].strip().split(';')
+
+        if u1 == user1:
+            appended_list.append([u1, u2, str(int(I1) - int(C))])
+            # fw.writelines('{};{}\t{}\n'.format(u1, u2, str(int(I1) - int(C))))
+
+        if u2 == user2:
+            appended_list.append([u2, u1, str(int(I2) - int(C))])
+            # fw.writelines('{};{}\t{}\n'.format(u2, u1, str(int(I2) - int(C))))
+
+    appended_list = sorted(appended_list, key=lambda x: x[0], reverse=True)
+
+    for i in appended_list:
+        fw.writelines('{};{}\t{}\n'.format(i[0], i[1], i[2]))
+
 
 I_mapper()
 I_reducer()
 
 Cuv_mapper()
 Cuv_reducer()
+
+Suv_mapper()
+Suv_reducer()
