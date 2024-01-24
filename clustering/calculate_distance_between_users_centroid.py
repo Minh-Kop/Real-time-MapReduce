9f523a24ced140da616b6f30597756b5b4656fa6
20127538 <87182623+Minh-Kop@users.noreply.github.com>
2024-01-24 20:11:25 +0700

    Update calculate distance between users and centroid function

--- clustering/calculate_distance_between_users_centroid.py
+++ clustering/calculate_distance_between_users_centroid.py
@@ -4,14 +4,21 @@ from mrjob.step import MRStep
 from mrjob.protocol import TextProtocol
 import numpy as np
 
+first_centroid_id = '1'
+first_centroid_value = '1'
+
 
 class DistanceBetweenUsersCentroid(MRJob):
     OUTPUT_PROTOCOL = TextProtocol
 
     def configure_args(self):
         super(DistanceBetweenUsersCentroid, self).configure_args()
-        self.add_file_arg('--init-centroid-path',
+        self.add_file_arg('--first-centroid-path',
                           help='Path to init centroid file')
+
+    def distance_between_users_centroid_mapper(self, _, line):
+        user, value = line.strip().split('\t')
+        yield f'{user}', f'{value}'
 
     def getInitCentroid(self, filename):
         f = open(filename, 'r')
@@ -19,42 +26,38 @@ class DistanceBetweenUsersCentroid(MRJob):
         f.close()
         return user
 
-    def distance_between_users_centroid_mapper_init(self):
-        init_centroid_path = self.options.init_centroid_path
-        result = self.getInitCentroid(init_centroid_path)
-        self.init_centroid_id = result[0]
-        self.init_centroid_value = result[1]
-
-    def distance_between_users_centroid_mapper(self, _, line):
-        user, value = line.strip().split('\t')
-        if user != self.init_centroid_id:
-            yield f'{user}', f'{value}'
+    def distance_between_users_centroid_reducer_init(self):
+        first_centroid_path = self.options.first_centroid_path
+        self.first_centroid_id, self.first_centroid_value = self.getInitCentroid(
+            first_centroid_path)
 
     def distance_between_users_centroid_reducer(self, user, value):
+        if user == self.first_centroid_id:
+            return
         value = list(value)[0].strip().split('|')
         value = [el.strip().split(';') for el in value]
         coordinate = np.array(value, dtype='f')
 
-        centroid_value = self.init_centroid_value
+        centroid_value = self.first_centroid_value
         centroid_value = centroid_value.strip().split('|')
         centroid_value = [el.strip().split(';') for el in centroid_value]
         centroid_coordinate = np.array(centroid_value, dtype='f')
 
         euclidean_distance = abs(np.linalg.norm(
             centroid_coordinate - coordinate))
-        yield user, euclidean_distance
+        yield user, f'{euclidean_distance}'
 
     def steps(self):
         return [
-            MRStep(mapper_init=self.distance_between_users_centroid_mapper_init,
-                   mapper=self.distance_between_users_centroid_mapper,
+            MRStep(mapper=self.distance_between_users_centroid_mapper,
+                   reducer_init=self.distance_between_users_centroid_reducer_init,
                    reducer=self.distance_between_users_centroid_reducer)
         ]
 
 
 if __name__ == '__main__':
     sys.argv[1:] = [
-        '--init-centroid', './init_centroid.txt',
-        './create_user_item_matrix.txt'
+        './output/user_item_matrix.txt',
+        '--first-centroid-path', './output/first_centroid.txt',
     ]
     DistanceBetweenUsersCentroid().run()
--- clustering/main.py
+++ clustering/main.py
@@ -6,6 +6,7 @@ from create_user_item_matrix import UserItemMatrix
 from create_importance import Importance
 from find_most_importance import MostImportance
 from create_first_centroid import FirstCentroid
+from calculate_distance_between_users_centroid import DistanceBetweenUsersCentroid
 from calculate_M_nearest_points import MNearestPoints
 
 if __name__ == '__main__':
@@ -82,6 +83,18 @@ if __name__ == '__main__':
     with mr_job.make_runner() as runner:
         runner.run()
         output_file = open('./output/first_centroid.txt', 'w')
+        for key, value in mr_job.parse_output(runner.cat_output()):
+            output_file.writelines(f'{key}\t{value}')
+        output_file.close()
+
+    # Calculate distance between users and centroid
+    mr_job = DistanceBetweenUsersCentroid(args=[
+        './output/user_item_matrix.txt',
+        '--first-centroid-path', './output/first_centroid.txt'
+    ])
+    with mr_job.make_runner() as runner:
+        runner.run()
+        output_file = open('./output/D.txt', 'w')
         for key, value in mr_job.parse_output(runner.cat_output()):
             output_file.writelines(f'{key}\t{value}')
         output_file.close()
