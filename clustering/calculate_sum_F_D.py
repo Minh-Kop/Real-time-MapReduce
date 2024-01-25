3dfd6511c5629bbd1a8efd0f92a0cb89fe993b09
20127506 <thoang1908@gmail.com>
2024-01-25 14:59:29 +0700

    calc F_D

---
+++ clustering/calculate_sum_F_D.py
@@ -0,0 +1,35 @@
+import sys
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import TextProtocol
+import numpy as np
+
+
+class SumFD(MRJob):
+    OUTPUT_PROTOCOL = TextProtocol
+
+    def sum_f_d_mapper(self, _, line):
+        user, value = line.strip().split('\t')
+        yield user, value
+
+    def sum_f_d_reducer(self, user, values):
+        values = [value.strip().split(';') for value in values]
+        values = np.array(values)
+        values = values.astype('f')
+        f_d = np.sum(values)
+
+        yield user, str(f_d)
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.sum_f_d_mapper,
+                   reducer=self.sum_f_d_reducer),
+        ]
+
+
+if __name__ == '__main__':
+    sys.argv[1:] = [
+        '../input_file.txt',  # Tệp đầu vào
+        # '--output', 'output1.txt'  # Tệp đầu ra
+    ]
+    SumFD().run()
