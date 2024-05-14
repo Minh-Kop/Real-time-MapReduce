b95d5b0505e8152c0c71e83b86854687556d2499
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2024-05-14 08:33:00 +0700

    feat: add code to measure F1-score in evaluation.py

--- eval_function.py
+++ eval_function.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-import math
+import numpy as np
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -22,110 +22,188 @@ def split_test_train(source_path, train_path, test_path, ratio=0.8):
         test_file.writelines(lines[ratio_index:])
 
 
-def evaluate(k, number_of_recommended_items, sim_path, train_path, test_path, avg_path):
-    # Read data from files
-    train_df = pd.read_csv(
+def evaluate(k, number_of_recommend_items, sim_path, train_path, test_path, avg_path):
+    # Create a training set DataFrame
+    training_df = pd.read_csv(
         train_path, delimiter="\t", names=["user_item", "rating_time"]
     )
+    training_df[["user", "item"]] = training_df["user_item"].str.split(";", expand=True)
+    training_df["rating"] = training_df["rating_time"].str.split(";", expand=True)[0]
+    training_df = training_df.drop(["user_item", "rating_time"], axis=1)
+    training_df = training_df.astype({"user": "int", "item": "int", "rating": "int"})
+
+    # Create a test set DataFrame
     test_df = pd.read_csv(test_path, delimiter="\t", names=["user_item", "rating_time"])
-    sim_df = pd.read_csv(sim_path, delimiter="\t", names=["user_user", "sim"])
-    avg_df = pd.read_csv(
-        avg_path, delimiter="\t", names=["user", "avg"], dtype={"user": str}
-    )
-
-    # Split user_item and rating_time columns into separate columns
-    train_df[["user", "item"]] = train_df["user_item"].str.split(";", expand=True)
-    train_df["rating"] = train_df["rating_time"].str.split(";", expand=True)[0]
-
     test_df[["user", "item"]] = test_df["user_item"].str.split(";", expand=True)
     test_df["rating"] = test_df["rating_time"].str.split(";", expand=True)[0]
-
-    sim_df[["curr_user", "user"]] = sim_df["user_user"].str.split(";", expand=True)
-
-    # Drop unnecessary columns
-    train_df = train_df.drop(["user_item", "rating_time"], axis=1)
     test_df = test_df.drop(["user_item", "rating_time"], axis=1)
+    test_df = test_df.astype(int)
+
+    # Create a similarity set
+    sim_df = pd.read_csv(sim_path, delimiter="\t", names=["user_user", "sim"])
+    sim_df[["user_1", "user_2"]] = sim_df["user_user"].str.split(";", expand=True)
     sim_df = sim_df.drop("user_user", axis=1)
-
-    # Merge average ratings into train_df and user_item_list
-    train_df = pd.merge(train_df, avg_df, on="user")
-
-    # Create user-item list with each record stores user and its item list
-    user_item_list = test_df.groupby("user")["item"].agg(list).reset_index()
-    user_item_list = pd.merge(user_item_list, avg_df, on="user")
-
-    # Create a dictionary to store similarities
-
-    sorted_df = (
-        sim_df.groupby("curr_user")
-        .apply(
-            lambda x: x.sort_values(by="sim", ascending=False)[:k], include_groups=False
-        )
+    sim_df = sim_df.astype({"user_1": "int", "user_2": "int"})
+    # Sort similarity based on user in similarity set
+    sorted_sim_df = (
+        sim_df.groupby("user_1")
+        .apply(lambda x: x.sort_values(by="sim", ascending=False), include_groups=False)
         .reset_index(level=1, drop=True)
-        .set_index(keys="user", append=True)
+        .set_index(keys="user_2", append=True)
     )
 
-    # Create a dataFrame to save predicted, observed value
-    val_df = pd.DataFrame(columns=["user", "item", "pre_rating"])
+    # Create an average rating set
+    avg_rating_df = pd.read_csv(avg_path, delimiter="\t", names=["user", "avg_rating"])
 
-    # Iterate over user_item_list
-    for _, row in user_item_list.iterrows():
-        u_user = row["user"]
-        items = row["item"]
-        avg = row["avg"]
+    # Merge average rating set into training set
+    training_df = pd.merge(training_df, avg_rating_df, on="user")
 
-        user_ratings = train_df[train_df["item"].isin(items)]
-        user_ratings = user_ratings[
-            user_ratings["user"].isin(sorted_df.loc[u_user].index)
+    # Create a test users set with each record stores an user's item list
+    test_users_df = test_df.groupby("user")["item"].agg(list).reset_index()
+    test_users_df = pd.merge(test_users_df, avg_rating_df, on="user")
+
+    # Create a dataFrame to save RMSE, F1 score
+    result_df = pd.DataFrame(
+        columns=[
+            "user",
+            "number_of_correct_predict_items",
+            "number_of_truth_set_items",
+            "RMSE",
+            "number_of_items",
         ]
-        # print(user_ratings['user'].drop_duplicates())
-
-        # Calculate similarities between the current user and all other users who have rated the items
-        user_ratings["similarity"] = user_ratings["user"].map(
-            lambda v_user: sorted_df.loc[(u_user, v_user), "sim"]
-        )
-
-        # Calculate the weighted sum of ratings and similarities
-        user_ratings["weighted_sum"] = (
-            user_ratings["rating"].astype(float) - user_ratings["avg"].astype(float)
-        ) * user_ratings["similarity"].astype(float)
-
-        # Sum of similarities for normalization
-        sum_df = (
-            user_ratings.groupby("item")
-            .agg({"similarity": "sum", "weighted_sum": "sum"})
-            .reset_index()
-        )
-        sum_df["pre_rating"] = avg + (sum_df["weighted_sum"] / sum_df["similarity"])
-        sum_df["user"] = u_user
-
-        val_df = pd.concat(
-            [val_df, sum_df.drop(["similarity", "weighted_sum"], axis=1)]
-        )
-    val_df = pd.merge(val_df, test_df, on=["user", "item"], how="left")
-    val_df.to_csv("./output/pre_obs_val", sep=",", index=False)
-
-    dif = (val_df["rating"].astype(float) - val_df["pre_rating"].astype(float)) ** 2
-    rmse = math.sqrt((dif / len(val_df)).sum())
-
-    val_df = pd.merge(val_df, avg_df, on="user").astype(float)
-
-    tp_df = (
-        val_df[val_df["pre_rating"] > val_df["avg"]]
-        .astype(float)
-        .drop("rating", axis=1)
     )
-    tp_df = pd.merge(tp_df, test_df.astype(float), on=["user", "item"])
 
-    tp = len(tp_df[tp_df["rating"] > tp_df["avg"]])
-    test_df = pd.merge(test_df, avg_df, on="user")
+    # Iterate over test users set
+    for _, row in test_users_df.iterrows():
+        user_1 = row["user"]
+        items = row["item"]
+        user_1_avg_rating = row["avg_rating"]
+        sorted_users_df = sorted_sim_df.loc[user_1]
 
-    precision = tp / len(val_df[val_df["pre_rating"] > val_df["avg"]])
-    recall = tp / len(test_df[test_df["rating"].astype(float) > test_df["avg"]])
+        # Check if the number of test set's items are enough to predict
+        if len(items) < number_of_recommend_items:
+            continue
 
-    f1 = 2 * (precision * recall) / (precision + recall)
+        predict_df = pd.DataFrame(columns=["item", "rating", "predict_rating", "rmse"])
 
-    return rmse, f1
+        for item in items:
+            # Filter training set DataFrame with only rows that have this item
+            filtered_training_df = training_df[training_df["item"] == item]
+
+            # Get top k users who rated this item
+            top_k_users_df = sorted_users_df[
+                sorted_users_df.index.isin(filtered_training_df["user"])
+            ][:k]
+
+            # Check if the number of top neighbors are enough to use for predict
+            if len(top_k_users_df) < k:
+                continue
+
+            # Continue filtering training set DataFrame with users in top k users
+            filtered_training_df = filtered_training_df[
+                filtered_training_df["user"].isin(top_k_users_df.index)
+            ]
+
+            # Add similarities between the current user and all other users in filtered training set
+            filtered_training_df["sim"] = filtered_training_df["user"].map(
+                lambda user_2: top_k_users_df.loc[user_2, "sim"]
+            )
+
+            # Calculate the weighted sum of ratings and similarities
+            filtered_training_df["weighted_error"] = (
+                filtered_training_df["rating"] - filtered_training_df["avg_rating"]
+            ) * filtered_training_df["sim"]
+
+            # Calculate predict rating
+            predict_rating = user_1_avg_rating + (
+                filtered_training_df["weighted_error"].sum()
+                / filtered_training_df["sim"].sum()
+            )
+            rating = test_df.loc[
+                (test_df["user"] == user_1) & (test_df["item"] == item), "rating"
+            ].values[0]
+            predict_df = pd.concat(
+                [
+                    predict_df,
+                    pd.DataFrame(
+                        [
+                            [
+                                item,
+                                rating,
+                                predict_rating,
+                                pow(predict_rating - rating, 2),
+                            ]
+                        ],
+                        columns=["item", "rating", "predict_rating", "rmse"],
+                    ),
+                ]
+            ).reset_index(drop=True)
+
+        # predict_df = predict_df[predict_df["predict_rating"] > user_1_avg_rating]
+        # predict_df = predict_df.sort_values(by="predict_rating", ascending=False)[
+        #     :number_of_recommend_items
+        # ]
+        recommend_item_df = predict_df[
+            predict_df["predict_rating"] > user_1_avg_rating
+        ].sort_values(by="predict_rating", ascending=False, ignore_index=True)[
+            :number_of_recommend_items
+        ][
+            ["item"]
+        ]
+
+        if len(recommend_item_df) < number_of_recommend_items:
+            continue
+
+        # Create truth set with only items with ratings > avg rating
+        test_user_df = test_df.loc[
+            (test_df["user"] == user_1) & (test_df["rating"] > user_1_avg_rating)
+        ]
+
+        # Merge with recommend set to get the correct predict items
+        merge_df = recommend_item_df.merge(
+            test_user_df,
+            on="item",
+        )
+        number_of_correct_predict_items = len(merge_df)
+        number_of_truth_set_items = len(test_user_df.index)
+
+        result_df = pd.concat(
+            [
+                result_df,
+                pd.DataFrame(
+                    [
+                        [
+                            user_1,
+                            number_of_correct_predict_items,
+                            number_of_truth_set_items,
+                            predict_df["rmse"].sum(),
+                            len(predict_df.index),
+                        ]
+                    ],
+                    columns=[
+                        "user",
+                        "number_of_correct_predict_items",
+                        "number_of_truth_set_items",
+                        "RMSE",
+                        "number_of_items",
+                    ],
+                ),
+            ]
+        ).reset_index(drop=True)
+
+    # Calculate RMSE
+    rmse = np.sqrt(result_df["RMSE"].sum() / result_df["number_of_items"].sum())
+
+    # Calculate F1-score
+    number_of_correct_predict_items = result_df["number_of_correct_predict_items"].sum()
+    number_of_recommend_items_ = number_of_recommend_items * len(result_df.index)
+    number_of_truth_set_items = result_df["number_of_truth_set_items"].sum()
+
+    precision = number_of_correct_predict_items / number_of_recommend_items_
+    recall = number_of_correct_predict_items / number_of_truth_set_items
+    f1_score = 2 * precision * recall / (precision + recall)
+
+    return rmse, f1_score
 
 
 if __name__ == "__main__":
@@ -141,4 +219,4 @@ if __name__ == "__main__":
     avg_file_path = "./input/avg-file.txt"
 
     RMSE, F1 = evaluate(10, 4, sim_path, train_file_path, test_file_path, avg_file_path)
-    print(RMSE, F1)
+    print(f"RMSE: {RMSE}\nF1-score: {F1}")
