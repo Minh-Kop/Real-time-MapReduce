29d6782e3a1de03da84d2e0621ed729e8556f172
20127506 <thoang1908@gmail.com>
2024-08-06 21:47:35 +0700

    fix: add path

--- split_train_test.py
+++ split_train_test.py
@@ -6,6 +6,12 @@ origin_file_path = "./input/input_file_1M.txt"
 # item_file path
 item_file_path = "./input/items_1M.txt"
 
+# output path
+input_train_path = "./input/input_file_1M_train.txt"
+input_test_path = "./input/input_file_1M_test.txt"
+item_train_path = "./input/items_1M_train.txt"
+item_test_path = "./input/items_1M_test.txt"
+
 # read input_file and item_file
 origin_rating_df = pd.read_csv(origin_file_path, names=["user_item_rating_time"])
 item_df = pd.read_csv(
@@ -39,9 +45,9 @@ item_file_test_df = pd.DataFrame(item_test_df["item"].unique(), columns=["item"]
 )
 
 # save train_input and test_input
-input_file_train_df.to_csv("./input/input_file_1M_train.txt", index=False, header=False)
-input_file_test_df.to_csv("./input/input_file_1M_test.txt", index=False, header=False)
+input_file_train_df.to_csv(input_train_path, index=False, header=False)
+input_file_test_df.to_csv(input_test_path, index=False, header=False)
 
 # save train_item and test_item
-item_file_train_df.to_csv("./input/items_1M_train.txt", index=False, header=False)
-item_file_test_df.to_csv("./input/items_1M_test.txt", index=False, header=False)
+item_file_train_df.to_csv(item_train_path, index=False, header=False)
+item_file_test_df.to_csv(item_test_path, index=False, header=False)
